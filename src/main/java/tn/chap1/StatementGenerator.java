e1efbdc2552496dc87efe6ad7dcdf0308209aeea
kamiri <khaled-amiri@hotmail.fr>
2023-10-31 17:15:55 +0100

    II- Decomposing the statement Function: tip1--> Refactoring changes the programs         in small steps, so if you         make a mistake, it is easy to         find where the bug is. tip2--> Any fool can write code that a         computer can understand.         Good programmers write code         that humans can understand.

--- src/main/java/tn/chap1/StatementGenerator.java
+++ src/main/java/tn/chap1/StatementGenerator.java
@@ -15,23 +15,7 @@ public class StatementGenerator {
             Play play = plays.get(perf.getPlayID());
             double thisAmount = 0;
 
-            switch (play.getType()) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.getAudience() > 30) {
-                        thisAmount += 1000 * (perf.getAudience() - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (perf.getAudience() - 20);
-                    }
-                    thisAmount += 300 * perf.getAudience();
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unknown type: " + play.getType());
-            }
+            thisAmount = amountFor(perf, play);
 
             // add volume credits
             volumeCredits += Math.max(perf.getAudience() - 30, 0);
@@ -48,6 +32,28 @@ public class StatementGenerator {
 
         result += "Amount owed is " + format.format(totalAmount / 100) + "\n";
         result += "You earned " + volumeCredits + " credits\n";
+        return result;
+    }
+
+    private static double amountFor(Performance aPerformance, Play play) {
+        double result;
+        switch (play.getType()) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.getAudience() > 30) {
+                    result += 1000 * (aPerformance.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.getAudience() > 20) {
+                    result += 10000 + 500 * (aPerformance.getAudience() - 20);
+                }
+                result += 300 * aPerformance.getAudience();
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown type: " + play.getType());
+        }
         return result;
     }
 }
